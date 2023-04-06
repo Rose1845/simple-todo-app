224144cf7fb90f59d67254f4ca6b164057d8ff93
Rose1845 <odhiamborose466@gmail.com>
2023-04-07 00:30:30 +0300

    simple card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  // const imageLink="https://v1.tailwindcss.com/img/card-top.jpg"
   return (
-    <React.Fragment>
-      <div>
-        <h1>Level 1</h1>
+    <div className="page-wrapper">
+      <div className="card-component">
+        <img
+          src="https://v1.tailwindcss.com/img/card-top.jpg"
+          alt=""
+          className="card-image"
+        />
+        <div className="card-content">
+          <h1 className="card-title">The Coldest Sunset</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas,
+            quasi. Quibusdam iste, quidem nihil laborum possimus aperiam
+            repellat dolor non totam consequuntur distinctio ducimus obcaecati
+            consequatur earum repudiandae sapiente cumque!
+          </p>
+          <div className="tags">
+            <span className="tag">#photography</span>
+            <span className="tag">#travel</span>
+            <span className="tag">#winter</span>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Level 2</h1>
-      </div>
-    </React.Fragment>
+    </div>
   );
 }
 
