423dac3ab08d3b17bfcf6aba01995d92ae0b4449
Rose1845 <odhiamborose466@gmail.com>
2023-04-06 22:35:43 +0300

    simple-tdo-app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Level 1</h1>
+      </div>
+      <div>
+        <h1>Level 2</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
